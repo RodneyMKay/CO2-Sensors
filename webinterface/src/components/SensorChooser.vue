110f3377939034ad90e5af1a39c23d93b73ff7f5
Carsten Stecker <stecker.carsten@web.de>
2021-03-04 20:04:08 +0100

    Added sensor Chooser Component

---
+++ webinterface/src/components/SensorChooser.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="SensorChooser">
+    <select name="Sensors" >
+      <option v-for="sensor in sensors"
+              :key="sensor">
+        {{sensor}}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SensorChooser",
+  props: {
+    'sensors': Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
