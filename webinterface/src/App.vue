86dd39848b6a00aece7d0f9bfe6c369bf63c5075
Carsten Stecker <stecker.carsten@web.de>
2021-03-04 20:04:58 +0100

    Added SensorChooser component to mein Component

--- webinterface/src/App.vue
+++ webinterface/src/App.vue
@@ -2,18 +2,36 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SensorChooser v-bind:sensors="sensors"></SensorChooser>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SensorChooser from "@/components/SensorChooser";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    SensorChooser
+  }, data: function () {
+    return {
+      sensors: ['name', 'name2']
+    }
   }
 }
+
+fetch("/api/sensors/list", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    }
+).then(response => response.json()).then(data => {
+  console.log('Success:', data);
+})
+
 </script>
 
 <style>
