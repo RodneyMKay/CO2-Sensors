2097c685bbb70d8fe22e51e7c3f67e2e0675b090
Carsten Stecker <stecker.carsten10@gmail.com>
2021-03-05 12:23:30 +0100

    added sensorTypes

--- webserver/constants.js
+++ webserver/constants.js
@@ -7,5 +7,10 @@ module.exports = {
         4: "hum",
         5: "pressure",
         6: "gas"
+    },
+    sensorTypes: {
+        1: "CCS811",
+        2: "BME280",
+        3: "BME680"
     }
 }
