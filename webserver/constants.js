f7556e486e24566faf38732cd9c8686754dafca3
Carsten Stecker <stecker.carsten10@gmail.com>
2021-03-05 09:19:08 +0100

    Created constants.js for backend

---
+++ webserver/constants.js
@@ -0,0 +1,10 @@
+module.exports = {
+    valueTypeIds: {
+        0: "eCO2",
+        1: "TVOC",
+        2: "TEMPERATURE",
+        3: "HUMIDITY",
+        4: "PRESSURE",
+        5: "GAS"
+    }
+}
