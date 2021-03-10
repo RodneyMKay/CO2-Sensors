0f94a8a12004bf8bc609a7c27c6a67ef65ffc342
Carsten Stecker <stecker.carsten10@gmail.com>
2021-03-10 11:17:33 +0100

    Added interactive Data to sensorCharts

--- webinterface/src/components/sensorChart.vue
+++ webinterface/src/components/sensorChart.vue
@@ -1,12 +1,12 @@
 <script>
-//import rest from "@/mixins/rest"
+import rest from "@/mixins/rest"
 import { Line, mixins } from 'vue-chartjs'
 const {reactiveData} = mixins;
 
 export default {
   name: "sensorChart",
   extends: Line,
-  mixins: [reactiveData],
+  mixins: [reactiveData, rest],
   props: {
     "sensor": {
       type: Object,
@@ -26,25 +26,86 @@ export default {
     update: function (val, oldVal) {
       val
       oldVal
-      this.fillData()
+      //this.fillData()
+      this.updateCharts(Date.now()-0.5*60*60*1000, Date.now())
     }
   },
   mounted () {
-    this.fillData();
+    this.updateCharts(Date.now()-0.5*60*60*1000, Date.now());
+    let min = null;
+    let max = null;
+    let unit = "";
+    if (this.sensor.valueType === 0) {
+      min = 300;
+      max = 1500;
+      unit = "ppm";
+    } else if (this.sensor.valueType === 1) {
+      min = 0;
+      max = 100;
+      unit = "none";
+    } else if (this.sensor.valueType === 2) {
+      min = 0;
+      max = 100;
+      unit = "IAQ";
+    } else if (this.sensor.valueType === 3) {
+      min = 0;
+      max = 40;
+      unit = "°C";
+    } else if (this.sensor.valueType === 4) {
+      min = 0;
+      max = 100;
+      unit = "%";
+    } else if (this.sensor.valueType === 5) {
+      min = 900;
+      max = 1100;
+      unit = "hPa";
+    } else if (this.sensor.valueType === 6) {
+      min = 0;
+      max = 10000;
+    } else if (this.sensor.valueType === 7) {
+      min = 300;
+      max = 1500;
+      unit = "ppm";
+    }
+    if (min!== null && max !== null) {
+      if (unit !== "") {
+        unit
+      }
+      this.options = {
+        scales: {
+          yAxes: [{
+            ticks: {
+              suggestedMin: min,
+              suggestedMax: max
+            }
+          }]
+        }
+      }
+    }
     this.renderChart(this.chartData, this.options);
   },
   methods: {
-    fillData () {
-      this.chartData = {
-        labels: ["8Uhr", "9Uhr", "10Uhr", "11Uhr", "12Uhr", "13Uhr", "14Uhr", "15Uhr", "16Uhr"],
-        datasets: [
-          {
-            label: "valueType" + this.sensor.valueType,
-            backgroundColor: 'rgba(' + this.getRandomInt(100, 255) +  ', ' + this.getRandomInt(100, 255) +  ', ' + this.getRandomInt(100, 255) +  ')',
-            data: [this.getRandomInt(0, 100), this.getRandomInt(0, 100), this.getRandomInt(0, 100), this.getRandomInt(0, 100), this.getRandomInt(0, 100), this.getRandomInt(0, 100), this.getRandomInt(0, 100), this.getRandomInt(0, 100), this.getRandomInt(0, 100)]
-          }
-        ]
-      }
+    // in milliseconds
+    updateCharts(startTimestamp, endTimestamp) {
+      let labels = []
+      let values = []
+
+      this.get(`/api/v1/sensors/${this.sensor.id}/data?from=${startTimestamp}&to=${endTimestamp}`).then(data => {
+        for (let i = 0; i < data.length; i++) {
+          labels.push(data[i].time)
+          values.push(data[i].value)
+        }
+        this.chartData = {
+          labels: labels,
+          datasets: [
+            {
+              label: "valueType" + this.sensor.valueType,
+              backgroundColor: 'rgba(' + this.getRandomInt(100, 255) +  ', ' + this.getRandomInt(100, 255) +  ', ' + this.getRandomInt(100, 255) +  ')',
+              data: values
+            }
+          ]
+        }
+      })
     },
     getRandomInt (start, end) {
       return Math.floor(Math.random() * (end-start)) + start
